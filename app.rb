4c3a807258ae33677e5a5425e3aee6f11a87a280
Alister <a.fenix@icloud.com>
2015-01-19 21:49:55 -0800

    Clean erb files to include better syntax.

--- app.rb
+++ app.rb
@@ -1,9 +1,8 @@
+require('pry')
 require('sinatra')
 require('sinatra/reloader')
 also_reload('./lib/**/*.rb')
 require('./lib/contact')
-require('pry')
-
 
 get('/') do
   @contacts = Contact.all()
@@ -18,22 +17,24 @@ post('/contacts') do
   erb(:index)
 end
 
-post('/new_number') do
+post('/new_numbers') do
   @new_number = params.fetch('new_number')
   @type = params.fetch('type')
   @new_number = Phone.new({:new_number => @new_number, :type => @type, :contact_id => @contact_id}).save()
-  @new_number = Contact.search(params.fetch('uid').to_i())
-  @contacts.add_phone(@new_number)
+  @contact = Contact.search(params.fetch('uid').to_i())
+  @contact.add_phone(@new_number)
+binding.pry
+  @contacts = Contact.all()
   @new_numbers = Phone.all()
-  erb(:contacts)
+  erb(:contact)
 end
 
 get('/new_numbers/:contact_id') do
-  @phone_numbers = Phone.find(params.fetch('contact_id').to_i())
-  erb(:new_numbers)
+  @phone_number = Phone.find(params.fetch('contact_id').to_i())
+  erb(:new_number)
 end
 
 get('/contacts/:uid') do
-  @contacts = Contact.search(params.fetch('uid').to_i())
-  erb(:contacts)
+  @contact = Contact.search(params.fetch('uid').to_i())
+  erb(:contact)
 end
